5c67b711af0034e71bc98c15ce4745925fd777f3
Caden Aragon <cadenaragon@gmail.com>
2021-11-03 16:00:16 -0600

    Chabges

--- spec/example/empty_string.rb
+++ spec/example/empty_string.rb
@@ -1,12 +1,3 @@
-require "rails_helper"
-
-RSpec.describe "The math below is right..." do
-  it "should equal 42" do
-    expect(6 * 7).to eq(42)
-  end
-end
-
-
 RSpec.describe "hello spec" do
     # ...
     describe String do
@@ -15,4 +6,4 @@ RSpec.describe "hello spec" do
         expect(string).to eq("")
       end
     end
-   end
+  end
